8b4711b4faadf2454cd2c93a560a3d474216c02c
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2021-06-20 13:05:20 -0300

    Added authenticate user unity test

---
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,80 @@
+import { AppError } from "../../../../shared/errors/AppError";
+import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+describe("Authenticate User", () => {
+
+  let userRepositoryInMemory: InMemoryUsersRepository;
+  let createUserUseCase: CreateUserUseCase;
+  let authenticateUserUseCase: AuthenticateUserUseCase;
+
+  beforeAll(() => {
+    userRepositoryInMemory = new InMemoryUsersRepository();
+    createUserUseCase = new CreateUserUseCase(userRepositoryInMemory);
+    authenticateUserUseCase = new AuthenticateUserUseCase(userRepositoryInMemory);
+  });
+
+  it("Should be able to authenticate", async () => {
+    await createUserUseCase.execute({
+      name: "User Test",
+      email: "user@test.com.br",
+      password: "1234",
+    });
+
+    const response = await authenticateUserUseCase.execute({
+      email: "user@test.com.br",
+      password: "1234",
+    });
+
+    expect(response).toHaveProperty("token");
+    expect(response).toHaveProperty("user");
+
+  });
+
+  it("Should not be able to authenticate a non-existing user", async () => {
+
+    expect(async () => {
+      await authenticateUserUseCase.execute({
+        email: "non-existing-user@test.com.br",
+        password: "1234",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+
+  });
+
+  it("Should not be able to authenticate user with incorrect pasaword", async () => {
+
+    expect(async () => {
+      await createUserUseCase.execute({
+        name: "User Test",
+        email: "user@test.com.br",
+        password: "1234"
+      });
+
+      await authenticateUserUseCase.execute({
+        email: "user@test.com.br",
+        password: "9999",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+
+  });
+
+  it("Should not be able to authenticate user with incorrect pasaword", async () => {
+
+    expect(async () => {
+      await createUserUseCase.execute({
+        name: "User Test",
+        email: "user@test.com.br",
+        password: "1234"
+      });
+
+      await authenticateUserUseCase.execute({
+        email: "incorrect-mail@test.com.br",
+        password: "1234",
+      });
+    }).rejects.toBeInstanceOf(AppError);
+
+  });
+
+});
