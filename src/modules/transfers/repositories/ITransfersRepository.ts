cdb9224aeb3454f2f452829e9d0cadc9f925b29c
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2021-07-02 20:48:36 -0300

    Removed Transfer module

--- src/modules/transfers/repositories/ITransfersRepository.ts
+++
@@ -1,8 +0,0 @@
-import { ICreateTransferDTO } from "../dto/ICreateTransferDTO";
-import { Transfer } from "../entities/Transfer";
-
-interface ITransfersRepository {
-  create(data: ICreateTransferDTO): Promise<Transfer>
-};
-
-export { ITransfersRepository };
