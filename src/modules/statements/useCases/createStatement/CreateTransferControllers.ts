9e25edb7d2f7fb62972431db3340e9e0e3d1eb1d
Guilherme Tadashii <guilhermetadashii.dev@gmail.com>
2021-07-03 14:34:05 -0300

    Added integration test for the transfer value feature

---
+++ src/modules/statements/useCases/createStatement/CreateTransferControllers.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { CreateStatementUseCase } from "./CreateStatementUseCase";
+
+enum OperationType {
+  DEPOSIT = "deposit",
+  WITHDRAW = "withdraw",
+  TRANSFER = "transfer",
+}
+
+class CreateTransferController {
+  async execute(request: Request, response: Response): Promise<Response> {
+    const { user_id } = request.params;
+    const { id: sender_id } = request.user;
+
+    const { amount, description } = request.body;
+
+    const type = "transfer" as OperationType;
+
+    const createStatement = container.resolve(CreateStatementUseCase);
+
+    const statement = await createStatement.execute({
+      user_id,
+      sender_id,
+      type,
+      amount,
+      description
+    });
+
+    return response.status(201).json(statement);
+  };
+};
+
+export { CreateTransferController };
